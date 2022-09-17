24f1445b085a4c3b5c92c17d7f6a5df8f4446b5b
kieblig76 <113736733+kieblig76@users.noreply.github.com>
2022-09-16 18:10:03 -0600

    Add files via upload

---
+++ Ejercicio3p1.java
@@ -0,0 +1,30 @@
+
+package ejercicio3p1;
+
+import java.util.Scanner;
+
+public class Ejercicio3p1 {
+
+ 
+    public static void main(String[] args) {
+      Scanner Entrada = new Scanner(System.in);
+      float nota1, nota2, nota3, promedio;
+      System.out.println("Ingrese la primera nota");
+      nota1 = Entrada.nextFloat();
+       System.out.println("Ingrese la segunda nota");
+      nota2 = Entrada.nextFloat();
+       System.out.println("Ingrese la tercera nota");
+      nota3 = Entrada.nextFloat();
+      promedio = (nota1 + nota2 + nota3) / 3;
+      System.out.println("El promedio es: " + promedio);
+      if (promedio >=7){
+          System.out.println("Promocionado");
+      } else if (promedio >= 4){
+          System.out.println("Regular");
+      } else {
+          System.out.println("Reprobado");
+      }
+      
+    }
+    
+}
