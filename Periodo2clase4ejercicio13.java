d9d63671d7f229c80e53d416bea48350ef43ead2
kieblig76 <113736733+kieblig76@users.noreply.github.com>
2022-09-16 18:16:50 -0600

    Add files via upload

---
+++ Periodo2clase4ejercicio13.java
@@ -0,0 +1,7 @@
+package com.mycompany.periodo2clase4ejercicio13;
+public class Periodo2clase4ejercicio13 {
+    public static void main(String[] args) {
+        for (int i=100;i>=1;i--)
+        System.out.println(i);
+    }
+}
