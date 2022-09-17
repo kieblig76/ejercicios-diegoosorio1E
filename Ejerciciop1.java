3c125e4b60bc74a19aba5d18375dad4e3ac94a91
kieblig76 <113736733+kieblig76@users.noreply.github.com>
2022-09-16 18:13:41 -0600

    Add files via upload

---
+++ Ejerciciop1.java
@@ -0,0 +1,27 @@
+
+package ejerciciop1;
+
+import java.util.Scanner;
+
+public class Ejerciciop1 {
+
+   
+    public static void main(String[] args) {
+     Scanner entrada = new Scanner(System.in);
+     
+     int numero1; 
+     int numero2; 
+     int suma; 
+     
+     System.out.print("Escriba el primer entero: ");
+     numero1 = entrada.nextInt();
+     
+     System.out.print("Escriba el segundo entero: ");
+     numero2 = entrada.nextInt();
+     
+     suma = numero1 + numero2;
+     
+     System.out.println("La suma es " + suma);
+    }
+    
+}
