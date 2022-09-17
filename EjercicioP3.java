974ae4656a1fbe36568392e530c9acf32fe4469d
kieblig76 <113736733+kieblig76@users.noreply.github.com>
2022-09-16 18:18:06 -0600

    Add files via upload

--- EjercicioP3.java
+++ EjercicioP3.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ejerciciop3;
+package EjercicioP3;
 
-/**
- *
- * @author diego
- */
-public class EjercicioP3 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class EjercicioP3{
     public static void main(String[] args) {
-        // TODO code application logic here
+        // se crear el objeto a la clase Scanner
+        Scanner ingresar=new Scanner (System.in);
+        //DECLARAR VARIABLES
+        double nota;
+        
+        System.out.println("favor ingresar un valor desde 1 hasta 10");
+        nota=ingresar.nextDouble();
+        
+        while(nota<1 | nota>10)
+        {
+            System.out.println("Lo siento mucho pero el valor proporcionado no es correcto");
+           nota=ingresar.nextDouble();
+        }
     }
-    
 }
