5da9a47579ef72315196381093c29e66a02b03ab
kieblig76 <113736733+kieblig76@users.noreply.github.com>
2022-09-16 18:21:01 -0600

    Add files via upload

---
+++ Periodo3clase4ejercicio17.java
@@ -0,0 +1,8 @@
+package com.mycompany.periodo3clase4ejercicio1;
+public class Periodo3clase4ejercicio17 {
+    public static void main(String[] args) {
+        for (char c='a'; c<= 'z'; c++) {
+        System.out.println("El caracter " + (int) c +"es: ");
+       }
+   }
+}
